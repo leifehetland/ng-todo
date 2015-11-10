a5c9080944911b7880a626c60754ab01df892d2f
Leif E. Hetland <lehetland1@gmail.com>
2015-11-09 19:45:35 -0600

    angular time pt1

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = angular.module('TodoApp', [])
+
+app.controller("TodoCtrl", function($scope){
+	$scope.title= "Welcome to your Todo App";
+})
+
+
+app.controller("PastaCtrl", function($scope){
+	$scope.pasta= "I love carbs";
+	$scope.foods="";
+})
