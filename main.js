43d72e1062100a07b4b4434261b494c7eb3f0f5f
Leif E. Hetland <lehetland1@gmail.com>
2015-11-11 16:47:37 -0600

    angular lessons

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ var app = angular.module('TodoApp', [])
 
 app.controller("TodoCtrl", function($scope){
 	$scope.title= "Welcome to your Todo App";
+
+	$scope.todos = [
+		{name: "Work On Angular", complete:"incomplete"},
+		{name: "Read a Chapter of a Book", complete:"incomplete"},
+		{name: "Sleep", complete:"complete"}
+		];
+	$scope.killTodo=function(todo){
+		var todoIndex=$scope.todos.indexOf(todo);
+
+		if(todoIndex>=0){
+			$scope.todos.splice(todoIndex, 1)
+		}
+	}
 })
 
 
